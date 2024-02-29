cf09d0f5ec0e7493f785ff8941931854dde8cf3c
Leonardo Capocci <leocap2001@gmail.com>
2024-02-29 14:06:54 -0800

    added image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import createMenuPage from './menu.js'
-import createContactPage from './contact.js'
+import './style.css';
+import createMenuPage from './menu.js';
+import createContactPage from './contact.js';
+import meat from './meat.png';
 
 const homeButton = document.querySelector('#home');
 const menuButton = document.querySelector('#menu');
@@ -14,12 +16,18 @@ function createHomePage() {
   const content = document.querySelector('#content');
   const header = document.createElement('h1');
   const description = document.createElement('p');
+  const image = new Image();
 
   header.textContent = 'Welcome to My Restaurant';
+
   description.textContent = 'Experience the finest dining at our restaurant. With our exquisite dishes and cozy ambiance, we guarantee a memorable culinary journey for every visitor.';
+
+  image.className = 'meat';
+  image.setAttribute('src', meat);
 
   content.appendChild(header);
   content.appendChild(description);
+  content.appendChild(image);
   }
 
 homeButton.addEventListener('click', () => {
