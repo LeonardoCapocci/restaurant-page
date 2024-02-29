8e70ab470feceff07b903ba7a47d5ae40b4c92d2
Leonardo Capocci <leocap2001@gmail.com>
2024-02-29 13:04:37 -0800

    reordered functions, so that the page isn't dependent on hoisting and for readability

--- src/index.js
+++ src/index.js
@@ -10,6 +10,18 @@ function clearContent() {
   content.textContent = ''
 }
 
+function createHomePage() {
+  const content = document.querySelector('#content');
+  const header = document.createElement('h1');
+  const description = document.createElement('p');
+
+  header.textContent = 'Welcome to My Restaurant';
+  description.textContent = 'Experience the finest dining at our restaurant. With our exquisite dishes and cozy ambiance, we guarantee a memorable culinary journey for every visitor.';
+
+  content.appendChild(header);
+  content.appendChild(description);
+  }
+
 homeButton.addEventListener('click', () => {
   clearContent();
   createHomePage();
@@ -24,17 +36,5 @@ contactButton.addEventListener('click', () => {
   clearContent();
   createContactPage();
 })
-
-function createHomePage() {
-  const content = document.querySelector('#content');
-  const header = document.createElement('h1');
-  const description = document.createElement('p');
-
-  header.textContent = 'Welcome to My Restaurant';
-  description.textContent = 'Experience the finest dining at our restaurant. With our exquisite dishes and cozy ambiance, we guarantee a memorable culinary journey for every visitor.';
-
-  content.appendChild(header);
-  content.appendChild(description);
-  }
 
 createHomePage();
